a9c5cf116377c3c38008e8639858399b22be2e85
Ali Ramzan <aramzan.pro@gmail.com>
2022-06-30 18:54:22 +0200

    first commit

---
+++ js/ArrayStorage.js
@@ -0,0 +1,36 @@
+'use strict'
+//Stockage locale de nos taches
+class ArrayStorage
+{
+
+    constructor(name){
+        this.name = name
+        this.list = this.get()
+    }
+
+
+    get() {
+    if (!localStorage.getItem(this.name)) {
+        localStorage.setItem(this.name, '[]')
+    }
+    return JSON.parse(localStorage.getItem(this.name))
+    }
+
+    set(value) {
+        this.list.push(value)
+        localStorage.setItem(this.name, JSON.stringify(this.list))
+
+    }
+
+    remove(value) {
+        const index = this.list.indexOf(value)
+        this.list.splice(index, 1)  
+        localStorage.setItem(this.name, JSON.stringify(this.list))
+
+
+    }
+
+    clear() {
+        localStorage.removeItem(this.name)
+    }
+}
---
+++ js/index.js
@@ -0,0 +1,78 @@
+'use strict'
+//DOM selection
+const list = document.getElementById('list')
+const input = document.getElementById('input')
+const add = document.getElementById('add')
+const clear = document.getElementById('clear')
+const url = document.getElementById('url')
+const load = document.getElementById('load')
+const storage = new ArrayStorage ('tasks')
+   
+const tasks = storage.list
+
+function taskToDOM(task) {
+    if (typeof task === 'string' && task) {
+        const li = document.createElement('li')
+        const remove = document.createElement('button')
+
+        li.textContent = task
+        remove.textContent = 'REMOVE'
+
+        remove.addEventListener('click', () =>{
+            const value = remove.parentNode.firstChild.textContent
+            storage.remove(value)
+            list.removeChild(remove.parentNode)
+        })
+
+        li.appendChild(remove)
+
+        list.insertBefore(li, list.firstChild)
+        return true
+    }
+    return false
+}
+
+tasks.forEach(task => taskToDOM(task))
+
+function newTask(params) {
+    if (storage.list.indexOf(input.value)=== -1 && taskToDOM(input.value)) {
+        storage.set(input.value)
+        input.value = ''
+    }
+    input.focus()
+}
+add.addEventListener('click',newTask)
+input.addEventListener('keydown',e => {
+    if(e.key === 'Enter') {
+        newTask()
+    }
+}
+)
+
+clear.addEventListener('click', () => {
+    storage.clear()
+    list.innerHTML = '' })
+
+load.addEventListener('click', () => {
+    fetch(url.value)
+    .then(response => {
+        if (response.ok) {
+            return response.json()
+        }
+        throw new Error(`${response.statusText} (${response.status})`)
+    })
+    .then(tasks => {
+        if (Array.isArray(tasks)) {
+            tasks.forEach(task => {
+                if (storage.list.indexOf(task)=== -1 && taskToDOM(task)) {
+                    storage.set(task)
+                   
+        }
+    }
+    )
+    return
+    }
+    throw new TypeError(`La Reponse nest pas un tableau JSON (type : $ {typeof}`)
+    }
+    )     
+})
